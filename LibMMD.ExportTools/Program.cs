e002a7b61731e8ebbda7f9853df647c793f736c8
xalcon <xalcon@xalcon.net>
2019-07-31 17:50:43 +0200

    add wip wavefront export

---
+++ LibMMD.ExportTools/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using CommandLine;
+using LibMMD.ExportTools.Wavefront;
+using LibMMD.Pmx;
+
+namespace LibMMD.ExportTools
+{
+    class Program
+    {
+        [Verb("wavefront", HelpText = "Export a pmx model into a wavefront obj")]
+        class WavefrontExportOptions
+        {
+            [Option('i', "input", Required = true)]
+            public string InputFilePath { get; set; }
+            [Option('o', "output", Required = false)]
+            public string OutputFilePath { get; set; }
+        }
+
+        static int Main(string[] args)
+        {
+            var result = Parser.Default.ParseArguments<WavefrontExportOptions>(args)
+                .WithParsed<WavefrontExportOptions>(ExportWavefrontModel);
+
+            return result.Tag == ParserResultType.Parsed ? 0 : -1;
+        }
+
+        private static void ExportWavefrontModel(WavefrontExportOptions obj)
+        {
+            var file = new FileInfo(obj.InputFilePath);
+            if (!file.Exists)
+            {
+                Console.Error.WriteLine("File not found: " + file.FullName);
+            }
+
+            using (var stream = file.OpenRead())
+            using (var outputStream = File.OpenWrite(file.FullName + ".obj"))
+            {
+                var model = PmxParser.Parse(stream);
+                PmxToWavefrontExporter.ExportModel(model, outputStream);
+            }
+        }
+    }
+}
---
+++ LibMMD.ExportTools/Wavefront/PmxToWavefrontExporter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using LibMMD.Pmx;
+
+namespace LibMMD.ExportTools.Wavefront
+{
+    internal class PmxToWavefrontExporter
+    {
+        public static void ExportModel(PmxModel pmx, Stream outputStream)
+        {
+            var oldCulture = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            try
+            {
+                var writer = new StreamWriter(outputStream);
+                writer.Write("# Model: ");
+                writer.WriteLine(pmx.ModelNameLocal);
+            
+                writer.WriteLine($"# vertex positions");
+                foreach(var vertex in pmx.Vertices)
+                    writer.WriteLine($"v {vertex.Position.X} {vertex.Position.Y} {vertex.Position.Z}");
+
+                writer.WriteLine($"# vertex normals");
+                foreach(var vertex in pmx.Vertices)
+                    writer.WriteLine($"vn {vertex.Normal.X} {vertex.Normal.Y} {vertex.Normal.Z}");
+            
+                writer.WriteLine($"# vertex uv");
+                foreach(var vertex in pmx.Vertices)
+                    writer.WriteLine($"vt {vertex.UV.X} {vertex.UV.Y}");
+
+                
+                writer.WriteLine($"# polygonal faces");
+                var mat = pmx.Materials.First();
+                var matIndex = 0;
+                var materialIndexCountOffset = 0;
+                writer.WriteLine($"# usemtl {mat.NameLocal}");
+                for (var i = 0; i < pmx.Indices.Count; i += 3)
+                {
+                    var idx0 = pmx.Indices[i];
+                    var idx1 = pmx.Indices[i + 1];
+                    var idx2 = pmx.Indices[i + 2];
+                    if (mat.IndexCount + materialIndexCountOffset < i)
+                    {
+                        materialIndexCountOffset += mat.IndexCount;
+                        mat = pmx.Materials[++matIndex];
+                        writer.WriteLine($"# usemtl {mat.NameLocal}");
+                    }
+                    writer.WriteLine($"f {idx0}/{idx0}/{idx0} {idx1}/{idx1}/{idx1} {idx2}/{idx2}/{idx2}");
+                }
+            }
+            finally
+            {
+                Thread.CurrentThread.CurrentCulture = oldCulture;
+            }
+        }
+
+        public static void ExportMaterial(PmxModel pmx, Stream outputStream)
+        {
+
+        }
+    }
+}
